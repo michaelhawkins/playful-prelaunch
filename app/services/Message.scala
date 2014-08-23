7973fd09e50b953af6549c25cb64546db84e1420
Michael Hawkins <mike@hawkinsunlimited.com>
2014-08-23 18:56:30 -0400

    update Message to include editable subject

---
+++ app/services/Message.scala
@@ -0,0 +1,39 @@
+package services
+import play.api.i18n._
+import play.api.libs.json._
+import play.api.libs.ws._
+import scala.concurrent.Future
+import play.api.Play.current
+
+object Message {
+
+  def buildRegistrationTemplate(firstName: String, lastName: String, email: String, mandrillKey: String): JsValue = {
+    JsObject(Seq(
+    //      "key" -> JsString("5t1Q3spLNNtrBesf3gxK7A"),*/
+          "key" -> JsString(play.Play.application.configuration.getString("mandrillKey")),
+          "template_name" -> JsString("Playful-Prelaunch-Contact-Us"),
+          "template_content" -> JsArray(Seq(
+            JsObject(Seq("name" -> JsString("firstname"), "content" -> JsString(firstName)), JsObject(Seq("name" -> JsString("appname)", "content" -> JsString("global.appName")))))),
+          "message" -> JsObject(
+            Seq("subject" -> JsString("Thank you for contacting us"),
+              "from_email" -> JsString(Messages("global.fromEmail")),
+              "from_name" -> JsString(Messages("global.companyName")),
+              "to" -> JsArray(Seq(
+                  JsObject(Seq("email" -> JsString(email), //this email var isn't working
+                  "name" -> JsString(firstName + " " + lastName),
+                  "type" -> JsString("to")
+                )))),
+          "headers" -> JsObject(Seq(
+            "Reply-To" -> JsString(Messages("global.replyToEmail")))),
+          "important" -> JsBoolean(false),
+          "track_opens" -> JsBoolean(true)))))
+  }
+
+  def emailNewRegistration(firstName: String, lastName: String, email: String) {
+    val mandrillKey: String = play.Play.application.configuration.getString("mandrillKey")
+    val jsonClass: JsValue = buildRegistrationTemplate(firstName, lastName, email, mandrillKey)
+
+    val apiUrl: String = play.Play.application.configuration.getString("mandrillSendViaTemplateURL")
+    val futureResponse: Future[WSResponse] = WS.url(apiUrl).post(jsonClass)
+  }
+}
--- build.sbt
+++ build.sbt
@@ -4,16 +4,16 @@ version := "1.0-SNAPSHOT"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
-scalaVersion := "2.11.0"
+scalaVersion := "2.11.2"
 
-val playVersion = "2.3.0"
+val playVersion = "2.3.2"
 
 libraryDependencies ++= Seq(
   jdbc,
   anorm,
   ws,
   "com.typesafe.play" %% "play" % playVersion,
-  "org.webjars" %% "webjars-play" % playVersion,
+  "org.webjars" %% "webjars-play" % "2.3.0",
   "org.postgresql" % "postgresql" % "9.3-1101-jdbc41",
   "org.seleniumhq.selenium" % "selenium-java" % "2.42.2",
   "org.webjars" % "bootstrap" % "3.1.1-2"
