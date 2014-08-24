0da0bdf4f5345e72b68928c58dc62ae6d9d8cc49
Michael Hawkins <mike@hawkinsunlimited.com>
2014-08-23 23:55:51 -0400

    update to resolve emails

--- app/services/Message.scala
+++ app/services/Message.scala
@@ -7,34 +7,34 @@ import play.api.Play.current
 
 object Message {
 
-  def buildRegistrationTemplate(firstName: String, lastName: String, email: String, mandrillKey: String): JsValue = {
-    JsObject(Seq(
-    //      "key" -> JsString("5t1Q3spLNNtrBesf3gxK7A"),*/
-          "key" -> JsString(mandrillKey),
-          "template_name" -> JsString("my-playful-prelaunch-registration"),
-          "template_content" -> JsArray(Seq(
-            JsObject(Seq("name" -> JsString("firstname"), "content" -> JsString(firstName))),
-            JsObject(Seq("name" -> JsString("appname"), "content" -> JsString("global.appName"))))),
-          "message" -> JsObject(
-            Seq("subject" -> JsString("Thank you for your interest in" + Messages("global.appName")),
-              "from_email" -> JsString(Messages("global.fromEmail")),
-              "from_name" -> JsString(Messages("global.companyName")),
-              "to" -> JsArray(Seq(
-                  JsObject(Seq("email" -> JsString(email), //this email var isn't working
-                  "name" -> JsString(firstName + " " + lastName),
-                  "type" -> JsString("to")
-                )))),
-          "headers" -> JsObject(Seq(
-            "Reply-To" -> JsString(Messages("global.replyToEmail")))),
-          "important" -> JsBoolean(false),
-          "track_opens" -> JsBoolean(true)))))
-  }
+def buildRegistrationTemplate(firstName: String, lastName: String, email: String, mandrillKey: String): JsValue = {
+  JsObject(Seq(
+  //      "key" -> JsString("5t1Q3spLNNtrBesf3gxK7A"),*/
+        "key" -> JsString(mandrillKey),
+        "template_name" -> JsString("my-playful-prelaunch-registration"),
+        "template_content" -> JsArray(Seq(
+          JsObject(Seq("name" -> JsString("firstname"), "content" -> JsString(firstName))),
+          JsObject(Seq("name" -> JsString("appname"), "content" -> JsString(Messages("global.appName")))))),
+        "message" -> JsObject(
+          Seq("subject" -> JsString("Thank you for your interest in" + Messages("global.appName")),
+            "from_email" -> JsString(Messages("global.fromEmail")),
+            "from_name" -> JsString(Messages("global.companyName")),
+            "to" -> JsArray(Seq(
+                JsObject(Seq("email" -> JsString(email),
+                "name" -> JsString(firstName + " " + lastName),
+                "type" -> JsString("to")
+              )))),
+        "headers" -> JsObject(Seq(
+          "Reply-To" -> JsString(Messages("global.replyToEmail")))),
+        "important" -> JsBoolean(false),
+        "track_opens" -> JsBoolean(true)))))
+}
 
   def emailNewRegistration(firstName: String, lastName: String, email: String) {
     val mandrillKey: String = play.Play.application.configuration.getString("mandrillKey")
-    val jsonClass: JsValue = buildRegistrationTemplate(firstName, lastName, email, mandrillKey)
+    val jsonClass = buildRegistrationTemplate(firstName, lastName, email, mandrillKey)
 
-    val apiUrl: String = play.Play.application.configuration.getString("mandrillSendViaTemplateURL")
+    val apiUrl = play.Play.application.configuration.getString("mandrillSendViaTemplateURL")
     val futureResponse: Future[WSResponse] = WS.url(apiUrl).post(jsonClass)
   }
 }
